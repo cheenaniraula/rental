3b824780ec09b5af0ef205e35e014a3837e327c3
Cheenaniraula <niraula010@gmail.com>
2019-06-23 20:46:30 +0545

    modified wsgi

--- rental_system/rental_system/wsgi.py
+++ rental_system/rental_system/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rental_system.settings')
+
+# application = get_wsgi_application()
 import os
+import sys
 
+path = os.path.expanduser('~/django_projects/rental_system')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rental_system.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rental_system.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
